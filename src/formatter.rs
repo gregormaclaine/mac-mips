c9a56c83adbd2fa669ad03b3560d5ed10945f4e5
Gregor Maclaine <thegregormaclaine@gmail.com>
2024-01-25 23:23:16 +0000

    Slight refactoring

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,19 +1,9 @@
 use std::fmt::Error;
 
-fn split_line_from_comment(line: &str) -> (&str, &str) {
-    if let Some(comment_index) = line.find('#') {
-        return (&line[..comment_index], &line[(comment_index + 1)..]);
-    } else {
-        return (line, "");
-    }
-}
-
-pub fn format(contents: String) -> Result<String, Error> {
-    let raw_lines: Vec<&str> = contents.lines().map(|l| l.trim()).collect();
-
+fn remove_redundant_lines<'a>(lines: &'a Vec<&'a str>) -> Vec<&'a str> {
     let mut line_blocks: Vec<Vec<&str>> = vec![Vec::new()];
 
-    for line in raw_lines {
+    for line in lines {
         if let Some(cur_block) = line_blocks.last_mut() {
             if line.is_empty() {
                 if cur_block.len() != 0 && !(cur_block[0].ends_with(":") && cur_block.len() == 1) {
@@ -45,38 +35,53 @@ pub fn format(contents: String) -> Result<String, Error> {
         }
     }
 
-    let mut lines: Vec<&str> = Vec::new();
+    let mut output: Vec<&str> = Vec::new();
 
     for block in line_blocks {
         if block.len() == 0 {
             continue;
         }
         for line in block {
-            lines.push(line);
+            output.push(line);
         }
-        lines.push("");
+        output.push("");
     }
 
-    let mut formatted_lines: Vec<String> = lines
-        .iter()
-        .map(|line| {
-            let (code, comment) = split_line_from_comment(line);
+    return output;
+}
 
-            let mut parts: Vec<String> = code.split_whitespace().map(|p| String::from(p)).collect();
-            for i in (0..parts.len()).rev() {
-                if parts[i] == "," {
-                    parts.remove(i);
-                    parts[i - 1] += ",";
-                }
-            }
+fn split_line_from_comment(line: &str) -> (&str, &str) {
+    if let Some(comment_index) = line.find('#') {
+        return (&line[..comment_index], &line[(comment_index + 1)..]);
+    } else {
+        return (line, "");
+    }
+}
 
-            if comment == "" {
-                return parts.join(" ");
-            } else {
-                return parts.join(" ") + "  # " + comment.trim();
-            }
-        })
-        .collect();
+fn format_line(line: &str) -> String {
+    let (code, comment) = split_line_from_comment(line);
+
+    let mut parts: Vec<String> = code.split_whitespace().map(|p| String::from(p)).collect();
+    for i in (0..parts.len()).rev() {
+        if parts[i] == "," {
+            parts.remove(i);
+            parts[i - 1] += ",";
+        }
+    }
+
+    if comment == "" {
+        return parts.join(" ");
+    } else {
+        return parts.join(" ") + "  # " + comment.trim();
+    }
+}
+
+pub fn format(contents: String) -> Result<String, Error> {
+    let raw_lines: Vec<&str> = contents.lines().map(|l| l.trim()).collect();
+
+    let lines = remove_redundant_lines(&raw_lines);
+
+    let mut formatted_lines: Vec<String> = lines.iter().map(|line| format_line(line)).collect();
 
     let mut after_bookmarks = false;
     for line in formatted_lines.iter_mut() {
